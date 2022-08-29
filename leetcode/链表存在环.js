17ab3c6477cfc73fe1d0623d0a8e2f42bcc1156b
jiangyanwei <1129499077@qq.com>
2022-08-29 23:03:07 +0800

    feat: 是否是环形链表

---
+++ leetcode/链表存在环.js
@@ -0,0 +1,41 @@
+
+
+//题目要求 每个链表节点的值 是值类型 
+/**
+ * 
+ * @param {*} head 
+ *解法: 遍历链表 判断每个元素是否出现过
+ * 
+ */
+const hasCyle = (head) => {
+   const cache = new Set();
+
+   while(head) {
+      if (cache.has(head.val)) {
+        return true
+      } else {
+        cache.add(head.val)
+      }
+      head = head.next;
+   }
+   return false;
+}
+
+/**
+ * 
+ * @param {*} head 
+ * 线性表(链表或者数组)  双指针解法
+ */
+const hasCyle2 = (head) => {
+   let slow = head;
+   let fast = head;
+
+   while(fast && fast.next) {
+      fast = fast.next.next;
+      slow = slo.next;
+      if (fast === slow) {
+         return true
+      }
+   }
+   return false;
+}
