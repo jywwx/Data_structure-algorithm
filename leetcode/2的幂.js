89a5212a2eb6173e8fe8ac08383bf901d951186e
jiangyanwei <1129499077@qq.com>
2022-08-30 23:38:38 +0800

    feat: JS位运算符

---
+++ leetcode/2的幂.js
@@ -0,0 +1,31 @@
+// 题目描述 给定一个整数 判断 n 是否是 2 的幂次方 n === X个2 相乘
+
+/**
+ * 
+ * @param {*} n 
+ */
+const isPoerOfTwo = (n) => {
+    while (n > 2) {
+        if (n%2 === 1) {
+          return false
+        }
+       n = Math.floor(n / 2);
+    }
+    return true;
+}
+
+
+/**
+ * 
+ * @param {*} n 
+ * @returns 
+ * 
+ * 原理解释  所有2 的幂次方的 二进制数字的特点 第一位是1 其余位是0  n - 1 的二进制特点 第一位是0 其余位是1  所以采用( n & n - 1) ==== 0来判断
+ */
+const isPoerOfTwo2 = (n) => {
+    return n > 0 && (n & (n - 1)) === 0
+}
+
+
+
+console.log(isPoerOfTwo(128),"isPoerOfTwo(128)")
