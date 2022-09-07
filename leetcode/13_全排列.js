6972fee863a19a2a8ef5c6a1eec488fbc16c4cce
jiangyanwei <jiangyanwei@biaoguoworks.com>
2022-09-07 20:17:07 +0800

    feat: 在二维数组重通过回溯搜索字符串

--- leetcode/13_全排列.js
+++ leetcode/13_全排列.js
@@ -25,4 +25,18 @@ var permute = function(nums) {
    backTrack(list,[],nums);
    return list;
 }
+
+// 回溯的全部路径的公式
+// let lsit = []
+// function backTrack(list,临时路径,输入) {
+//  结束条件:
+//    临时路径，新增一个路径
+//    循环 {
+//        选择一个数据   (选择其他数据);
+//        递归 backTrack (list,临时路径,输入)
+//        撤回选择的数据
+//    }
+//  }
+
+
 console.log(permute([1,2,3]))
---
+++ leetcode/14_单词搜索.js
@@ -0,0 +1,73 @@
+
+/**
+ * 
+ * @param {*} board 
+ * @param {*} word 
+ */
+
+/**
+ * 输入:
+ * board = [ 
+ * ['A'，'B','C','E']
+ * ['S'，'F','C','S']
+ * ['A'，'D','E','E']
+ * ]
+ * word = ['ABCCED'];
+ * 输出: bool
+ * 
+ * 
+ */
+const board = [
+    ['A','B','C','E'],
+    ['S','F','C','S'],
+    ['A','D','E','E']
+],word = 'ABCCED';
+var exist = function (board,word) {
+    if (board.length === 0) {
+       return false;
+    }
+
+    if (word.length === 0) {
+       return true;
+    }
+
+    const Row = board.length;
+    const Col = board[0].length;
+
+    for (let i = 0; i < Row; i ++) {
+       for (let j = 0; j < Col; j ++) {
+          const ret = find(i, j, 0);
+          if (ret) {
+            return ret;
+          }
+       }
+    } 
+    return false;
+
+    function find(i, j, cur) {
+       if (i >= Row || i < 0) {
+          return false;
+       }
+       if (j >= Col || j < 0) {
+          return false;
+       }
+
+       const letter = board[i][j];
+
+       if (letter !== word[cur]) {
+         return false;
+       }
+
+       if (cur === word.length - 1) {
+          return true;
+       }
+       board[i][j] = null;
+       const ret = find(i + 1,j,cur + 1) || find(i - 1,j,cur + 1) || find(i,j + 1,cur + 1) || find(i,j - 1,cur + 1);
+       board[i][j] = letter;
+       return ret;
+    }
+ 
+}
+console.log(exist(board,word))
+
+// 解题思路公式
