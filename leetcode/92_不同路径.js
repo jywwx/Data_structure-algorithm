1f1b5aed91a81b6b55ec28612a0ba12148936310
jywwx <1129499077@qq.com>
2022-11-01 22:08:34 +0800

    不同路径

---
+++ leetcode/92_不同路径.js
@@ -0,0 +1,27 @@
+/**
+ * 一个机器人位于一个 m x n 网格的左上角(起始点在网格的左上角记为'start')
+ *  机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角
+ * 记为finish
+ * 
+ * 问总共有多好条不同的路径？
+ */
+
+var uniquePaths = function (m,n) {
+   const dp = Array(m).fill('').map((i) => Array(n).fill(''));
+
+   for (let i = 0; i < m; i++) {
+      dp[i][0] = 1;
+   }
+
+   for (let i = 0; i < n; i ++) {
+      dp[0][i] = 1;
+   }
+   
+   for (let i = 1; i < m; i ++) {
+      for (let j = 1; j < n; j ++) {
+        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+      }
+   }
+   
+   return dp[m-1][n-1];
+}
