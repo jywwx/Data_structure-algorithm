c207ceba69a5465c997b5b646fdba6079051517d
jiangyanwei <jiangyanwei@biaoguoworks.com>
2022-12-12 15:13:19 +0800

    feat: 最大子数组之和(非连续)

---
+++ leetcode/102_最长连续递增序列.js
@@ -0,0 +1,11 @@
+/**
+ * 给定一个未经排序的整数数组,找到最长且连续递增的子序列，并返回该序列的长度。
+ * 
+ * 连续递增的子序列 可以由两个下标l 和 r（l < r） 确定，如果对于每个 l <= i < r,
+ * 都有nums[i] < nums[i + 1], 那么子序列 [nums[l],nums[l + 1],...,nums[r - 1],nums[r]]
+ * 就是连续递增子序列。
+ */
+
+var findLengthOfLCIS = function (nums) {
+
+}
