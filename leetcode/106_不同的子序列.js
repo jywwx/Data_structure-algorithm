20d4dd3ed6bc3c7c32b5862028ef3672661801ab
jywwx <1129499077@qq.com>
2022-12-19 21:59:17 +0800

    feat: 不同子序列

---
+++ leetcode/106_不同的子序列.js
@@ -0,0 +1,33 @@
+/**
+ * 给定一个字符串s 和 一个字符串t， 计算在s 的子序列 中 t 出现的个数。
+ */
+var numDistinct = function (s, t) {
+   // 遍历s
+   // 遍历t
+   // dp[i][j] s 中以i 结尾的子序列中,出现t中j之前的字符串的个数为dp[i][j]
+   
+   // babgbag
+   // bag
+
+   // s[i] t[j] 相等
+   //   用s[i - 1] 之前的字符串匹配 dp[i - 1][j - 1];
+   //   不用s[i - 1] 之前dp[i - 1][j]
+   // s[i] t[j]相等
+   // s[i - 1] 来匹配 d[i - 1][j]
+
+   let dp = Array(s.length + 1).fill(0).map((item) => Array(t.length).fill(0));
+   for (let i = 0; i < s.length; i ++) {
+     dp[i][0] = 1;
+   }
+
+   for (let i = 0; i < s.length; i ++) {
+      for (let j = 0; j < t.length; j ++) {
+        if (s[i - 1] === t[j - 1]) {
+            dp[i][j] = dp[i][j - 1] + dp[i -1][j];
+        } else {
+            dp[i][j] = dp[i - 1][j];
+        }
+      }
+   }
+   return dp[s.length][t.length];
+}
