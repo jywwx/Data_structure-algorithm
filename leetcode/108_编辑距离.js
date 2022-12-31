75486aa5dd5c476a7c95ceb65142f3d31be9f3a2
jiangyanwei <jiangyanwei@biaoguoworks.com>
2022-12-31 19:46:17 +0800

    feat: 编辑距离修正

--- leetcode/108_编辑距离.js
+++ leetcode/108_编辑距离.js
@@ -1,37 +1,38 @@
 /**
  * 给你两个单词word1 和word2，请返回将word1 转换成word2
  * 所使用的最少操作数。
- * 
+ *
  * 你可以对一个单词进行如下三种操作：
- * 
+ *
  *   插入一个字符
  *   删除一个字符
  *   转换一个字符
  */
-var minDistance = function (word1,word2) {
-    
-    const dp = Array(word1.length + 1).fill(0).map((i) => Array(word2.length + 1).fill(0));
+var minDistance = function (word1, word2) {
+  const dp = Array(word1.length + 1)
+    .fill(0)
+    .map((i) => Array(word2.length + 1).fill(0));
 
-    for (let i = 1; i <= word1.length; i ++) {
-       dp[i][0] = 1;
-    }
+  for (let i = 1; i <= word1.length; i++) {
+    dp[i][0] = 1;
+  }
 
-    for (let j = 1; j <= word2.length; j ++) {
-       dp[0][j] = 1;
-    }
+  for (let j = 1; j <= word2.length; j++) {
+    dp[0][j] = 1;
+  }
 
-    for (let i = 1; i <= word1.length; i ++) {
-       for (let j = 1; j <= word2.length; j ++) {
-           if (word1[i - 1] === word2[j - 1]) {
-              dp[i][j] = dp[i - 1][j - 1];
-           } else {
-               dp[i][j] = Math.min(
-                dp[i - 1][j - 1],
-                dp[i][j - 1],
-                dp[i - 1][l]
-               )
-           }
-       }
+  for (let i = 1; i <= word1.length; i++) {
+    for (let j = 1; j <= word2.length; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(
+          dp[i - 1][j - 1] + 1,
+          dp[i][j - 1] + 1,
+          dp[i - 1][l] + 1
+        );
+      }
     }
-    return dp[word1.length][word2.length];
-}
+  }
+  return dp[word1.length][word2.length];
+};
